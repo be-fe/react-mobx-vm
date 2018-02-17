8c12355320d1b154932d637ca6e7304fe2398836
Cuttle Cong <moyuyc95@gmail.com>
2018-02-17 21:37:53 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -6,28 +6,28 @@
  */
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
-
+import commonjs from 'rollup-plugin-commonjs'
 
 export default [
   {
-    input: 'src/index.js',
+    input: 'lib/index.js',
     output: {
       file: 'dist/react-mobx-vm.min.js',
       format: 'umd',
       name: 'react-mobx-vm'
     },
     external: [
-      id => {
-        console.log(id)
-        return true
-      }
+      'react',
+      'mobx',
+      'mobx-react'
     ],
     plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        runtimeHelpers: true
-      }),
-      resolve()
+      resolve(),
+      commonjs(),
+      // babel({
+      //   exclude: 'node_modules/**',
+      //   runtimeHelpers: false
+      // })
     ]
   }
 ]
