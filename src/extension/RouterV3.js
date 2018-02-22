42898dfed84761fbe8ffe2637f314c3cff5d666d
ganyanlin <ganyanlin@baidu.com>
2018-02-22 16:47:45 +0800

    huanhangxiugai

--- src/extension/RouterV3.js
+++ src/extension/RouterV3.js
@@ -35,8 +35,8 @@ function render(props) {
 
 
 /**
- * 如果你依赖了 **react-router v3**，那么可以使用该 Router 定义，而不是 react-router 的 Router
- * <br>**注意** `import Router from 'react-mobx-vm/extension/RouterV3'`
+ * 如果你依赖了 **react-router v3**，那么可以使用该 Router 定义，而不是 react-router 的 Router  <br>
+ * **注意** `import Router from 'react-mobx-vm/extension/RouterV3'`
  * @public
  * @name Router
  */
