4db92367a40c1cd776e89b923bc23553d0efa381
Cuttle Cong <moyuyc95@gmail.com>
2018-02-22 21:18:27 +0800

    fix: packages/RouterV3 supports props.createElement

--- srcPackages/RouterV3.js
+++ srcPackages/RouterV3.js
@@ -8,31 +8,6 @@ import * as React from 'react'
 import { h } from '../'
 import { RouterContext, Router } from 'react-router'
 
-function render(props) {
-  const components = props.components.map(comp => {
-    if (comp && typeof comp === 'object') {
-      const f = () => {
-      }
-      f['__[[actual]]__'] = comp
-      return f
-    }
-    return comp
-  })
-  return (
-    <RouterContext
-      {...props}
-      components={components}
-      createElement={function (model, props = {}) {
-        const { children, ...rest } = props || {}
-        if (model['__[[actual]]__']) {
-          model = model['__[[actual]]__']
-        }
-        return h.call(this, model, rest, children)
-      }}
-    />
-  )
-}
-
 
 /**
  * 如果你依赖了 **react-router v3**，那么可以使用该 Router 定义，而不是 react-router 的 Router
@@ -43,5 +18,32 @@ function render(props) {
  * @name Router
  */
 export default function VMRouter(props = {}) {
+  const createElement = props.createElement || h
+  
+  function render(props) {
+    const components = props.components.map(comp => {
+      if (comp && typeof comp === 'object') {
+        const f = () => {
+        }
+        f['__[[actual]]__'] = comp
+        return f
+      }
+      return comp
+    })
+    return (
+      <RouterContext
+        {...props}
+        components={components}
+        createElement={function (model, props = {}) {
+          const { children, ...rest } = props || {}
+          if (model['__[[actual]]__']) {
+            model = model['__[[actual]]__']
+          }
+          return createElement.call(this, model, rest, children)
+        }}
+      />
+    )
+  }
+
   return <Router {...props} render={render} />
 }
