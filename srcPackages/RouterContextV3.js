147b725f10f9d8984a383e7617bba062ea33e392
Cuttle Cong <moyuyc95@gmail.com>
2018-03-02 22:25:57 +0800

    refactor: add RouterContext

---
+++ srcPackages/RouterContextV3.js
@@ -0,0 +1,45 @@
+/**
+ * @file Router
+ * @author Cuttle Cong
+ * @date 2018/1/27
+ * @description
+ */
+import * as React from 'react'
+import { h } from '../'
+import { RouterContext } from 'react-router'
+
+/**
+ * 如果你依赖了 **react-router v3**，那么可以使用该 RouterContext 定义，而不是 react-router 的 RouterContext
+ * **注意**: 请用以下方式引入
+ * `import RouterContext from 'react-mobx-vm/packages/RouterContextV3'`
+ * @public
+ * @name RouterContext
+ */
+export default function RouterContextV3({ components, createElement, ...props }) {
+  createElement = createElement || h
+  if (components) {
+    components = components.map(comp => {
+      if (comp && typeof comp === 'object') {
+        const f = () => {
+        }
+        f['__[[actual]]__'] = comp
+        return f
+      }
+      return comp
+    })
+  }
+
+  return (
+    <RouterContext
+      components={components}
+      createElement={function (model, props = {}) {
+        const { children, ...rest } = props || {}
+        if (model['__[[actual]]__']) {
+          model = model['__[[actual]]__']
+        }
+        return createElement.call(this, model, rest, children)
+      }}
+      {...props}
+    />
+  )
+}
--- srcPackages/RouterV3.js
+++ srcPackages/RouterV3.js
@@ -6,43 +6,20 @@
  */
 import * as React from 'react'
 import { h } from '../'
-import { RouterContext, Router } from 'react-router'
-
+import { Router } from 'react-router'
+import RouterContext from './RouterContextV3'
 
 /**
- * 如果你依赖了 **react-router v3**，那么可以使用该 Router 定义，而不是 react-router 的 Router  
+ * 如果你依赖了 **react-router v3**，那么可以使用该 Router 定义，而不是 react-router 的 Router
  * **注意**: 请用以下方式引入
  * `import Router from 'react-mobx-vm/packages/RouterV3'`
  * @public
  * @name Router
  */
 export default function VMRouter(props = {}) {
-  const createElement = props.createElement || h
-  
   function render(props) {
-    const components = props.components.map(comp => {
-      if (comp && typeof comp === 'object') {
-        const f = () => {
-        }
-        f['__[[actual]]__'] = comp
-        return f
-      }
-      return comp
-    })
-    return (
-      <RouterContext
-        {...props}
-        components={components}
-        createElement={function (model, props = {}) {
-          const { children, ...rest } = props || {}
-          if (model['__[[actual]]__']) {
-            model = model['__[[actual]]__']
-          }
-          return createElement.call(this, model, rest, children)
-        }}
-      />
-    )
+    return <RouterContext {...props} />
   }
 
-  return <Router {...props} render={render} />
+  return <Router render={render} {...props} />
 }
