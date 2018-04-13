1a982b7c8eceffd000b0b377a74652bdbb53a17d
imcuttle <moyuyc95@gmail.com>
2018-04-13 12:47:23 +0800

    chore: using size-limit

---
+++ src/Model/List.js
@@ -0,0 +1,14 @@
+/**
+ * @file List
+ * @author Cuttle Cong
+ * @date 2018/4/12
+ * @description
+ */
+
+import { IObservableArray } from 'mobx'
+// import Root from './Root'
+
+// TODO
+export default class List extends IObservableArray {
+
+}
